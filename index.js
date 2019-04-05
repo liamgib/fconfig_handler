582221baa9325b281ce5ed4b853d42b210b03649
liamgib <40730821+liamgib@users.noreply.github.com>
2019-04-05 17:51:18 +1000

    removed promise and set return to instance

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const jsonData = require("read-write-json");
+const fs = require('fs');
 const filetypes = ["json"];
+
+//Configuration
 let logErrors = true;
+let allowCreation = true;
+
+//Global variables
 let filename, type;
+let error_code = false;
+
 
 /**
  * This is the primary function when this instance is created.
@@ -29,20 +36,28 @@ module.exports = function main(name, values, config){
       if(config["error_logs"] == false){
         logErrors = false;
       }
-
+      if(config["allowCreation"] == false){
+        allowCreation = false;
+      }
     } else if(typeof config !== 'undefined') throw 'expected_object';
 
     //Process payload
     if(typeof values ==='object'){
-      
+      let file_existing = loadData();
+      if(file_existing == null){
+        if(!allowCreation) throw 'file_does_not_exist';
+      };
     } else throw 'expected_object';
-
+    return module.exports;
   } catch(e) {
     if(e === 'expected_object') error("expected variable type Object.");
     if(e === 'expected_string') error("expected variable type String.");
     if(e === 'invalid_filename') error("invalid file name provided.");
     if(e === 'invalid_filetype') error("unexpected filetype provided.");
+    if(e === 'file_does_not_exist') error("the file doesn't exist and allowCreation is set to false.");
+    error_code = e;
 
+    return module.exports;
   }
 }
 
@@ -50,15 +65,30 @@ module.exports.newConfigData = function newConfigData(){
   return new require("./configInstance");
 };
 
-module.exports.loadData = function loadData(fileName){
+
+module.exports.getErrorCode = function getErrorCode(){
+  return error_code;
+}
+
+module.exports.clearErrorCode = function clearErrorCode(){
+  error_code = false;
+  return module.exports;
+}
+
+module.exports.saveFile = function saveFile(){
+  fs.writeFileSync(filename, JSON.stringify(data, null, 2));
+}
+
+function loadData(){
   try {
-    return jsonData.readJSONFile(fileName);
-  } catch (err) {
+  let loaded_file = JSON.parse(fs.readFileSync(filename, 'utf8'));
+    return loaded_file
+  } catch(e){
     return null;
   }
 };
 
-
 function error(...message){
   if(logErrors) console.log('[fconfig_handler] ' + message);
 }
+
